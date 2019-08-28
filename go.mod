d559d5d9b10c9468956e2a8a03c1f7551fba574d
Skip Baney <twelvelabs@gmail.com>
2019-08-28 15:54:26 -0500

    Update to climbcomp-go 1.1.0; Implement MetaAPI

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/climbcomp/climbcomp-api
 
 go 1.12
 
-require github.com/climbcomp/climbcomp-go v1.0.4
+require (
+	github.com/climbcomp/climbcomp-go v1.1.0
+	github.com/golang/protobuf v1.3.1 // indirect
+	google.golang.org/grpc v1.19.1
+)
--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net"
 
 	"github.com/climbcomp/climbcomp-go/climbcomp"
+	meta_pb "github.com/climbcomp/climbcomp-go/climbcomp/meta/v1"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	msg := fmt.Sprintf("Climbcomp API v%v", climbcomp.VERSION)
-	fmt.Println(msg)
+	address := "0.0.0.0:3000"
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Could not listen on %v: %v", address, err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	meta_pb.RegisterMetaAPIServer(grpcServer, newMetaAPIServer())
+
+	log.Println("Server starting...")
+	log.Fatal(grpcServer.Serve(listener))
+}
+
+func newMetaAPIServer() *metaAPIServer {
+	return &metaAPIServer{}
+}
+
+type metaAPIServer struct {
+}
+
+func (s *metaAPIServer) GetVersion(ctx context.Context, req *meta_pb.GetVersionRequest) (*meta_pb.GetVersionResponse, error) {
+	resp := &meta_pb.GetVersionResponse{
+		Version: climbcomp.VERSION,
+	}
+	return resp, nil
 }
